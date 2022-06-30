83d6bb60822ecd412030ad3529a1873267055ead
motwnb <yangxvanming@126.com>
2022-06-30 21:54:19 +0800

    添加main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+from pygame.locals import *
+
+pygame.init()  # 初始化
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption('木锄启动器:Wooden Hoe Initiator(WHI)')
+
+
+
+
